5c505014400b5c40da4201a77a9e792acf69dedf
jw <cjw0515@naver.com>
2019-06-09 17:25:36 +0900

    테스트 코드

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
 
 # 커스텀 헤더
 request_header = {
@@ -14,8 +16,20 @@ request_header = {
 getParams = (('k1', 'v1'), ('k1', 'v1'), ('k1', 'v1'))
 getParams2 = dict([('k1', 'v1'), ('k1', 'v3'), ('k2', 'v2')])
 
+
+# get 요청
 response = requests.get('http://httpbin.org/get', params=getParams)
-print(response.json())
+# print(response.json())
+# print(response.text)
+# print(response.headers['content-type'])
+
+# JSON POST 요청
+jsonData = {'k1': 'v2', 'k2': [1,2,3], 'name': 'rednooby'}
+
+jsonString = json.dumps(jsonData)
+response = requests.post('http://httpbin.org/post', data=jsonString)
+
+print(response.text)
 
 # response = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 # response = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn', headers=request_header, params=getParams)
@@ -30,7 +44,7 @@ movies = list()
 for tag in result:
     movies.append(tag.text.strip())
 
-print(movies[0])
+# print(movies[0])
 
 # for item in movies:
 #     print(item)
