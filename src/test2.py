025abe49a600778bf348f9340b0a042a180a892b
jw <cjw0515@naver.com>
2019-06-26 00:11:48 +0900

    테스트코드

---
+++ src/test2.py
@@ -0,0 +1,26 @@
+import sqlite3 as lite
+import time
+
+database_filename = 'test.db'
+conn = lite.connect(database_filename)
+cs = conn.cursor()
+
+#drop table
+query = "DROP TABLE IF EXISTS t1"
+cs.execute(query)
+
+
+
+#create table
+query = "CREATE TABLE IF NOT EXISTS t1 (id INTEGER PRIMARY_KEY NOT_NULL, name VARCHAR(255), at DATETIME)"
+cs.execute(query)
+
+#insert table
+chars = "abcdefghijklmnopqrstuvwxyz"
+for a in range(len(chars)):
+    query = "INSERT into t1 values (?, ? , DATETIME('NOW'))"
+    cs.execute(query,(a, chars[a]))
+conn.commit()
+# closig
+cs.close()
+conn.close()
