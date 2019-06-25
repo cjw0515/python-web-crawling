7e20109604020ec3630b986bf07b734b3ca8a129
jw <cjw0515@naver.com>
2019-06-26 00:11:39 +0900

    모델 추가

---
+++ src/models/bestitem_1300k.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BestItem:
+    category: str = None
+    rank: int = None
+    imageURL: str = None
+    itemCode: int = None
+    brand: str = None
+    itemName: str = None
+    price: int = None
+    salePrice: int = None
+    numOfReview: int = None
+    numOfLike: int = None
