997a24c24333ea1af8cf6ed1762d6396ceea8383
jw <cjw0515@naver.com>
2019-06-27 23:58:57 +0900

    크롤러 개선

--- src/crawler/htmlcrawler.py
+++ src/crawler/htmlcrawler.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import sqlite_db
-import common_fn
+import common
+import re
+from urllib import parse
 
 def get_html(url):
     options = webdriver.ChromeOptions()
@@ -61,7 +63,6 @@ def crawl_1300k_best(html):
     item_list = soup.select('.gc_glst > li')
 
     # 같은 용도의 객체이나, 마크업이 다른것이 소수일 경우 메인로직에서 연산을 줄이기 위한 별도 처리과정 필요
-
     list_for_replace = [
         {'tag': 'span', 'old': 'gc_gpr_del', 'new': 'gpr_del'},
         {'tag': 'span', 'old': 'gc_gpr_sale', 'new': 'gpr_sale'}
@@ -69,63 +70,60 @@ def crawl_1300k_best(html):
     best2to5 = replace_attr(soup.select('.bst_rank .rank_bx2 li'), list_for_replace)
     bset1 = replace_attr(soup.select('.bst_rank .rank_bx1'), list_for_replace)
 
-    # best1상품 태그 삽입
-    new_tag1 = soup.new_tag('span', attrs={"class": "ico_rank"})
-    new_tag1.string = "1"
-    new_tag2 = soup.new_tag('span', attrs={'class': 'gimg', 'gimg': 'http://imgc.1300k.com/aaaaaib/goods/215024/59/215024596347.jpg?3'})
+    # best1 상품 태그 삽입
+    for item in bset1:
+        new_tag = soup.new_tag('span', attrs={"class": "ico_rank"})
+        new_tag.string = "1"
+        item.append(new_tag)
+        item.append(soup.new_tag('span', attrs={'class': 'gimg', 'gimg': item('img')[0].attrs['src']}))
 
-
-
-    bset1 = insert_tag(bset1, [
-        new_tag1,
-        new_tag2
-    ])
-    best2to5 = insert_tag(best2to5, [
-        new_tag2
-    ])
+    # best2~5상품 태그 삽입
+    for item in best2to5:
+        item.append(soup.new_tag('span', attrs={'class': 'gimg', 'gimg': item('img')[0].attrs['src']}))
 
     item_list = item_list + best2to5
     item_list = item_list + bset1
 
     item_arr = []
-    tmp_price_dic = {'Price': None, 'SalePrice': None}
 
-    # print(item_list)
+    for item in item_list:
+        category = '1'
+        rank = item('span', {'class': 'ico_rank'})[0].text
+        image_URL = item('span', {'class': 'gimg'})[0].attrs['gimg']
+        url = parse.urlparse(item.find('a', href=re.compile('goodsDetail.html?')).attrs['href'])
+        item_code = parse.parse_qs(url.query)['f_goodsno'][0]
+        brand = item('a', {'class': 'a_bname'})[0].text
+        item_name = item('a', {'class': 'a_gname'})[0].text
+        tmp_price_dic = get_item_price(item('span', {'class': 'gprice'})[0])
+        price = tmp_price_dic['Price']
+        sale_price = tmp_price_dic['SalePrice']
+        num_of_review = 3235
+        num_of_like = 2342
 
-    # for item in item_list:
-    #     category =  '1'
-    #     rank = item('span', {'class': 'ico_rank'})[0].text
-    #     image_URL = item('span', {'class': 'gimg'})[0].attrs['gimg']
-    #     item_code = item('span', {'class': 'gimg'})[0].attrs['gno']
-    #     brand = item('a', {'class': 'a_bname'})[0].text
-    #     item_name = item('a', {'class': 'a_gname'})[0].text
-    #     tmp_price_dic = get_item_price(item('span', {'class': 'gprice'})[0])
-    #     price = tmp_price_dic['Price']
-    #     sale_price = tmp_price_dic['SalePrice']
-    #     num_of_review = 3235
-    #     num_of_like = 2342
-    #
-    #     tmp_obj = (
-    #         category
-    #         , rank
-    #         , image_URL
-    #         , item_code
-    #         , brand
-    #         , item_name
-    #         , price
-    #         , sale_price
-    #         , num_of_review
-    #         , num_of_like
-    #     )
-    #     item_arr.append(tmp_obj)
-    #
-    # return item_arr
+        tmp_obj = (
+            category
+            , rank
+            , image_URL
+            , item_code
+            , brand
+            , item_name
+            , price
+            , sale_price
+            , num_of_review
+            , num_of_like
+        )
+        item_arr.append(tmp_obj)
+
+    return item_arr
 
 
 def insert_tag(obj, new_tag_list):
-    obj[0].append(new_tag_list[0])
-    obj[1].append(new_tag_list[0])
-    obj[2].append(new_tag_list[0])
+    idx = 0
+
+    for item in obj:
+        # print(tag)
+        item.append(new_tag_list[idx])
+        idx = idx + 1
 
     return obj
 
@@ -142,10 +140,10 @@ def replace_attr(obj, param_list):
 def get_item_price(price_tag_obj):
     price = {'Price': None, 'SalePrice': None}
     if price_tag_obj('span', {'class': 'dcprice'}):
-        price['Price'] = common_fn.get_integer(price_tag_obj('span', {'class': 'gpr_del'})[0].text)
-        price['SalePrice'] = common_fn.get_integer(price_tag_obj('span', {'class': 'gpr_sale'})[0].text)
+        price['Price'] = common.get_integer(price_tag_obj('span', {'class': 'gpr_del'})[0].text)
+        price['SalePrice'] = common.get_integer(price_tag_obj('span', {'class': 'gpr_sale'})[0].text)
     else:
-        price['Price'] = common_fn.get_integer(price_tag_obj('span', {'class': 'gpr_orig'})[0].text)
+        price['Price'] = common.get_integer(price_tag_obj('span', {'class': 'gpr_orig'})[0].text)
         price['SalePrice'] = None
 
     return price
@@ -156,11 +154,11 @@ if __name__ == "__main__":
     # item_list = soup.select('.gc_glst > li')
     # img = soup.select('.gds_img img')[0]['src']
     tmp = soup.select('.bst_rank .rank_bx2 li')
-    new_tag2 = soup.new_tag('span', attrs={'class': 'gimg',
-                                           'gimg': 'http://imgc.1300k.com/aaaaaib/goods/215024/59/215024596347.jpg?3'})
-    tmp = insert_tag(tmp, [new_tag2])
 
-    print(tmp)
+    for item in tmp:
+        url = parse.urlparse(item.find('a', href=re.compile('goodsDetail.html?')).attrs['href'])
+        print(parse.parse_qs(url.query)['f_goodsno'][0])
+
 
 
 
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     html = htmlcrawler.get_html('http://www.1300k.com/shop/best/best.html?f_bid=HD4001')
     data = htmlcrawler.crawl_1300k_best(html)
     print(data)
-    # htmlcrawler.insert_data(db_path="test.db", data=data)
+    htmlcrawler.insert_data(db_path="test.db", data=data)
 
 
 if __name__ == "__main__":
