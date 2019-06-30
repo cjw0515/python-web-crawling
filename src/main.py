659c37ee5ad8ea764c07613c34d4eec52e9d2003
jw <cjw0515@naver.com>
2019-07-01 01:45:41 +0900

    메인함수 수정

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-import htmlcrawler
+import crawler_1300k
 import time
 
 
 def main():
     start = time.time()
-    html = htmlcrawler.get_html('http://www.1300k.com/shop/best/best.html?f_bid=HD4001')
-    data = htmlcrawler.crawl_1300k_best(html)
-    htmlcrawler.insert_data(db_path="test.db", data=data)
+    # html = crawler_1300k.get_html('http://www.1300k.com/shop/best/best.html?f_bid=HD4001')
+    # data = crawler_1300k.crawl_1300k_best(html)
+    # crawler_1300k.insert_data(db_path="test.db", data=data)
+    print(crawler_1300k.get_category())
     end_time = time.time() - start
 
     print(end_time)
+
 
 if __name__ == "__main__":
     main()
