ee061b1aee3e6af1118fc4ac9415c72767ecf7f7
JONGWONCHOI <cjw0515@naver.com>
2019-07-03 18:02:36 +0900

    이미지 다운로드 추가

--- src/common/common.py
+++ src/common/common.py
@@ -1,2 +1,11 @@
+import requests
+
+
 def get_integer(s):
     return int(''.join(filter(str.isdigit, s)))
+
+
+def get_items_searchapi(keyword):
+    response = requests.get('http://wapi.10x10.co.kr/search/getitemlist.asp?q={0}'.format(keyword))
+
+    return response.text
--- src/test.py
+++ src/test.py
@@ -2,13 +2,16 @@ import sqlite_db
 import bestitem_1300k
 import textdistance
 import requests
+import cv2 as cv
+import numpy as np
+import common as com
+import shutil
 
 
 if __name__ == '__main__':
     sql = """
     select *
   from best100_1300k
-
     """
     db = sqlite_db.SqliteDatabase('1300k_best.db')
     # tmp_obj = bestitem_1300k.BestItem(
@@ -25,13 +28,17 @@ if __name__ == '__main__':
     # )
     result = db.query(sql)
     img_url = result[0][3]
+    response = requests.get(img_url, stream=True)
+    # save_image_to_file(response, dirname, index)
+    with open('image1.jpg', "wb") as image:
+        image.write(response.content)
+
     # print(result[0][3])
     # print(textdistance.jaro.normalized_similarity('고온어도시락현미밥식단시즌3 12팩', '고온어도시락 현미밥식단 시즌3'))
-    response = requests.get("http://wapi.10x10.co.kr/search/getitemlist.asp?q=dffd")
-    api_result = response.text
-    # print(response.text)
+    # api_result = com.get_items_searchapi('양산')
+    # print(api_result)
 
-    items = []
-    items = api_result.split("\n")
-    print(items[0])
+    # items = []
+    # items = api_result.split("\n")
+    # print(items[0])
 
