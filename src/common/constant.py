9b78e72efbd5d8a5f74bac3d43235fa13d442884
JONGWONCHOI <cjw0515@naver.com>
2019-07-09 16:15:10 +0900

    api 주소 추가

--- src/common/constant.py
+++ src/common/constant.py
@@ -1,4 +1,4 @@
 
-
+TEM_SEARCH_API = "http://wapi.10x10.co.kr/search/getitemlist.asp"
 IMG_1300k_DIR = r"C:\BA\python-web-crawling\src\images\1300k"
 IMG_10x10_DIR = r"C:\BA\python-web-crawling\src\images\10x10"
