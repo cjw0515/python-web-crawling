3dad3dfb926d91b6399cc3abc661e1760235febb
JONGWONCHOI <cjw0515@naver.com>
2019-07-09 18:00:40 +0900

    테스트 모듈 변경

--- src/test3.py
+++ src/test3.py
@@ -1,50 +1,32 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-from skimage import img_as_float
-from skimage.measure import compare_ssim as ssim
+from skimage import data, color
+from skimage.transform import rescale, resize, downscale_local_mean
 
+image = color.rgb2gray(data.astronaut())
 
-img = img_as_float(data.camera())
-rows, cols = img.shape
+image_rescaled = rescale(image, 1.0 / 4.0, anti_aliasing=False)
+image_resized = resize(image, (image.shape[0] / 4, image.shape[1] / 4),
+                       anti_aliasing=True)
+image_downscaled = downscale_local_mean(image, (4, 3))
 
-noise = np.ones_like(img) * 0.2 * (img.max() - img.min())
-noise[np.random.random(size=noise.shape) > 0.5] *= -1
+fig, axes = plt.subplots(nrows=2, ncols=2)
 
-def mse(x, y):
-    return np.linalg.norm(x - y)
-
-img_noise = img + noise
-img_const = img + abs(noise)
-
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4),
-                         sharex=True, sharey=True)
 ax = axes.ravel()
 
-mse_none = mse(img, img)
-ssim_none = ssim(img, img, data_range=img.max() - img.min())
+ax[0].imshow(image, cmap='gray')
+ax[0].set_title("Original image")
 
-mse_noise = mse(img, img_noise)
-ssim_noise = ssim(img, img_noise,
-                  data_range=img_noise.max() - img_noise.min())
+ax[1].imshow(image_rescaled, cmap='gray')
+ax[1].set_title("Rescaled image (aliasing)")
 
-mse_const = mse(img, img_const)
-ssim_const = ssim(img, img_const,
-                  data_range=img_const.max() - img_const.min())
+ax[2].imshow(image_resized, cmap='gray')
+ax[2].set_title("Resized image (no aliasing)")
 
-label = 'MSE: {:.2f}, SSIM: {:.2f}'
+ax[3].imshow(image_downscaled, cmap='gray')
+ax[3].set_title("Downscaled image (no aliasing)")
 
-ax[0].imshow(img, cmap=plt.cm.gray, vmin=0, vmax=1)
-ax[0].set_xlabel(label.format(mse_none, ssim_none))
-ax[0].set_title('Original image')
-
-ax[1].imshow(img_noise, cmap=plt.cm.gray, vmin=0, vmax=1)
-ax[1].set_xlabel(label.format(mse_noise, ssim_noise))
-ax[1].set_title('Image with noise')
-
-ax[2].imshow(img_const, cmap=plt.cm.gray, vmin=0, vmax=1)
-ax[2].set_xlabel(label.format(mse_const, ssim_const))
-ax[2].set_title('Image plus constant')
-
+ax[0].set_xlim(0, 512)
+ax[0].set_ylim(512, 0)
 plt.tight_layout()
 plt.show()
