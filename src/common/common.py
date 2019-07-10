786d8d6c86f0bfa985d74f20779f154e0bf0c159
JONGWONCHOI <cjw0515@naver.com>
2019-07-10 18:02:25 +0900

    테스트코드 추가

--- src/common/common.py
+++ src/common/common.py
@@ -1,8 +1,6 @@
 import requests
+import textdistance
 from constant import TEM_SEARCH_API
-import constant
-import os
-
 
 def get_integer(s):
     return int(''.join(filter(str.isdigit, s)))
@@ -14,8 +12,5 @@ def get_items_ten_api(keyword):
     return response.text
 
 
-if __name__ == "__main__":
-    file_list1 = os.listdir(constant.IMG_1300k_DIR)
-    file_list2 = os.listdir(constant.IMG_10x10_DIR)
-    print(file_list1)
-    print(file_list2)
+def string_match(str1, str2):
+    return textdistance.jaro.normalized_similarity(str1, str2)
