c1b0addadda261f8d2062f72933402d8e178a7ab
JONGWONCHOI <cjw0515@naver.com>
2019-07-11 17:19:24 +0900

    상수값 추가

--- src/common/constant.py
+++ src/common/constant.py
@@ -1,6 +1,8 @@
 import os
 
-PROJECT_ROOT_PATH = os.path.abspath('../../')
+PROJECT_ROOT_PATH = os.path.abspath('../')
 TEM_SEARCH_API = "http://wapi.10x10.co.kr/search/getitemlist.asp"
+URL_1300K_BEST = "http://www.1300k.com/shop/best/best.html"
 IMG_1300k_DIR = os.path.join(PROJECT_ROOT_PATH, r"src\images\1300k")
 IMG_10x10_DIR = os.path.join(PROJECT_ROOT_PATH, r"src\images\10x10")
+DB_1300K_BEST_PATH = os.path.join(PROJECT_ROOT_PATH, r"src\1300k_best.db")
