47a482a9987669d191674efa0d52efeb8b4506b8
JONGWONCHOI <cjw0515@naver.com>
2019-07-11 17:19:43 +0900

    애니메이션 클래스 추가

--- src/common/common.py
+++ src/common/common.py
@@ -1,6 +1,9 @@
 import requests
 import textdistance
+import time
+import sys
 from constant import TEM_SEARCH_API
+
 
 def get_integer(s):
     return int(''.join(filter(str.isdigit, s)))
@@ -10,6 +13,21 @@ def get_items_ten_api(keyword):
     response = requests.get(TEM_SEARCH_API+'?q={keyword}'.format(keyword=keyword))
 
     return response.text
+
+
+class Waiting:
+    def __init__(self):
+        self.done = False
+
+    def print_waiting(self):
+        animation = "|/-"
+
+        i = 0
+        while not self.done:
+            time.sleep(0.1)
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            i += 1
+            sys.stdout.flush()
 
 
 def string_match(str1, str2):
